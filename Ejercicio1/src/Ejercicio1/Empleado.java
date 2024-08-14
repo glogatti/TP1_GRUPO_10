f5b11c8479edd87660a6b6ba960c5bfcd81ffc26
Lucho <luchoguit@gmail.com>
2024-08-14 15:05:54 -0300

    + Encapsulamiento

--- Ejercicio1/src/Ejercicio1/Empleado.java
+++ Ejercicio1/src/Ejercicio1/Empleado.java
@@ -2,10 +2,39 @@ package Ejercicio1;
 
 public class Empleado {
 
+	//atributos
+	
 	private int id;
 	
 	private String nombre;
 	
 	private int edad;
+
+	//Getters y Setters
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 
 }
