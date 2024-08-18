9be4de1037bf6d4e01c756476172a31ee3100bb1
Lucho <luchoguit@gmail.com>
2024-08-18 15:43:22 -0300

    + Main

---
+++ Ejercicio1/src/Ejercicio1/Principal.java
@@ -0,0 +1,13 @@
+package Ejercicio1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Empleado objEmpleado = new Empleado();
+		
+		System.out.println(objEmpleado.toString());
+
+	}
+
+}
