5e2b836865d04d5c3ff05115b60b3f22b33b319a
Jose Arias <jose.arias@alumnos.frgp.utn.edu.ar>
2024-08-18 23:03:31 -0300

    Agrego un objeto empleado con un constructor que recibe parametros

--- Ejercicio1/src/Ejercicio1/Principal.java
+++ Ejercicio1/src/Ejercicio1/Principal.java
@@ -4,9 +4,17 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		
+		// Empleado 1
 		Empleado objEmpleado = new Empleado();
 		
 		System.out.println(objEmpleado.toString());
+		
+		//Empleado 2
+		
+		Empleado objEmpleado2 = new Empleado("jose",27 );
+		
+		System.out.println(objEmpleado2.toString());
 
 	}
 
